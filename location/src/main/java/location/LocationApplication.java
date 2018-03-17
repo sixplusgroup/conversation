63cda05f8f6aeb9737c9c65c9cd11c5d8ded4793
xufan.zhang <sunshinezxf@hotmail.com>
2018-03-17 18:20:34 +0800

    add location

--- location/src/main/java/location/LocationApplication.java
+++ location/src/main/java/location/LocationApplication.java
@@ -1,12 +1,25 @@
 package location;
 
+import finley.gmair.util.ResultData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@RequestMapping("/location")
 public class LocationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LocationApplication.class, args);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/address/resolve")
+    public ResultData geocoder(String address) {
+        ResultData result = new ResultData();
+        
+        return result;
     }
 }
