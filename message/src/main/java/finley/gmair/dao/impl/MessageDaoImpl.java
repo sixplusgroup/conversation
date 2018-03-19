9e8ef3b337d3fcdc7cab2ea64e82abc1fb8a11fb
hush <295065492@qq.com>
2018-03-19 12:36:29 +0800

    实现message相关接口

--- message/src/main/java/finley/gmair/dao/impl/MessageDaoImpl.java
+++ message/src/main/java/finley/gmair/dao/impl/MessageDaoImpl.java
@@ -3,24 +3,46 @@ package finley.gmair.dao.impl;
 import finley.gmair.dao.BaseDao;
 import finley.gmair.dao.MessageDao;
 import finley.gmair.model.message.TextMessage;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class MessageDaoImpl extends BaseDao implements MessageDao {
+
+    private Object lock = new Object();
+
     @Override
     public ResultData insert(TextMessage message) {
         ResultData result = new ResultData();
-
+        synchronized (lock) {
+            try {
+                sqlSession.insert("gmair.message.insert", message);
+                result.setData(message);
+            } catch (Exception e) {
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                result.setDescription(e.getMessage());
+            }
+        }
         return result;
     }
 
     @Override
     public ResultData query(Map<String, Object> condition) {
         ResultData result = new ResultData();
-
+        try {
+            List<TextMessage> list = sqlSession.selectList("gmair.message.query", condition);
+            if (list.isEmpty()) {
+                result.setResponseCode(ResponseCode.RESPONSE_NULL);
+            }
+            result.setData(list);
+        } catch (Exception e) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        }
         return result;
     }
 }
--- message/src/main/java/finley/gmair/service/impl/MessageServiceImpl.java
+++ message/src/main/java/finley/gmair/service/impl/MessageServiceImpl.java
@@ -1,25 +1,45 @@
 package finley.gmair.service.impl;
 
+import finley.gmair.dao.MessageDao;
 import finley.gmair.model.message.TextMessage;
 import finley.gmair.service.MessageService;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class MessageServiceImpl implements MessageService {
+    @Autowired
+    private MessageDao messageDao;
+
     @Override
     public ResultData createTextMessage(TextMessage message) {
         ResultData result = new ResultData();
-
+        ResultData response = messageDao.insert(message);
+        result.setResponseCode(response.getResponseCode());
+        if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            result.setData(response.getData());
+        } else {
+            result.setDescription(response.getDescription());
+        }
         return result;
     }
 
     @Override
     public ResultData fetchTextMessage(Map<String, Object> condition) {
         ResultData result = new ResultData();
-
-        return result;
+        ResultData response = messageDao.query(condition);
+        result.setResponseCode(response.getResponseCode());
+        if (response.getResponseCode() != ResponseCode.RESPONSE_OK) {
+            return response;
+        } else {
+            List<TextMessage> list = (List<TextMessage>) response.getData();
+            result.setData(list);
+            return result;
+        }
     }
 }
