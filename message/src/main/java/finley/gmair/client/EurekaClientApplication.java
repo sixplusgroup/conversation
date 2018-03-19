5f495dff71d551579ad077561669f52eccab21d3
xufan.zhang <sunshinezxf@hotmail.com>
2018-03-19 20:47:08 +0800

    record message to database

--- message/src/main/java/finley/gmair/client/EurekaClientApplication.java
+++ message/src/main/java/finley/gmair/client/EurekaClientApplication.java
@@ -1,16 +1,25 @@
 package finley.gmair.client;
 
+import com.netflix.discovery.converters.Auto;
 import finley.gmair.form.message.MessageForm;
+import finley.gmair.model.message.TextMessage;
+import finley.gmair.service.MessageService;
 import finley.gmair.util.MessageUtil;
 import finley.gmair.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@ComponentScan({"finley.gmair.service", "finley.gmair.dao"})
 @RestController
 @EnableDiscoveryClient
 @EnableEurekaClient
@@ -20,6 +29,9 @@ public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
+
+    @Autowired
+    private MessageService messageService;
 
     /**
      * This method is called to send message to a target user
@@ -33,6 +45,9 @@ public class EurekaClientApplication {
     public ResultData sendOne(MessageForm form) {
         ResultData result = new ResultData();
         MessageUtil.sendOne(form.getPhone().trim(), form.getText().trim());
+        //save the message to database asynchronously
+        TextMessage message = new TextMessage(form.getPhone().trim(), form.getText().trim());
+        new Thread(() -> messageService.createTextMessage(message)).start();
         return result;
     }
 
@@ -48,6 +63,12 @@ public class EurekaClientApplication {
     public ResultData sendGroup(MessageForm form) {
         ResultData result = new ResultData();
         MessageUtil.sendGroup(form.getPhone().trim(), form.getText().trim());
+        //save the message to database asynchronously
+        String[] phones = form.getPhone().split(",");
+        Arrays.stream(phones).forEach(item -> {
+            TextMessage message = new TextMessage(item.trim(), form.getText().trim());
+            new Thread(() -> messageService.createTextMessage(message)).start();
+        });
         return result;
     }
 }
