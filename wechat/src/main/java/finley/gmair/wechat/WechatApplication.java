12984cfadda1bc11c5a2d2c3f5fc07d344616d02
hush <295065492@qq.com>
2018-04-07 19:12:22 +0800

    debug

--- wechat/src/main/java/finley/gmair/wechat/WechatApplication.java
+++ wechat/src/main/java/finley/gmair/wechat/WechatApplication.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@RequestMapping("/wechat")
 @SpringBootApplication
 @RestController
 @ComponentScan({"finley.gmair.scheduler", "finley.gmair.service", "finley.gmair.dao", "finley.gmair.controller"})
