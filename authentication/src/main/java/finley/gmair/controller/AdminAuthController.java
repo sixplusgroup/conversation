269a225c35c1a19ec665afe037a647270c924bfe
xufan.zhang <sunshinezxf@hotmail.com>
2018-04-11 14:58:42 +0800

    add interface to create a administrator

--- authentication/src/main/java/finley/gmair/controller/AdminAuthController.java
+++ authentication/src/main/java/finley/gmair/controller/AdminAuthController.java
@@ -1,6 +1,9 @@
 package finley.gmair.controller;
 
+import com.alibaba.fastjson.JSON;
+import finley.gmair.form.admin.AdminForm;
 import finley.gmair.form.admin.LoginForm;
+import finley.gmair.model.admin.Admin;
 import finley.gmair.service.AdminService;
 import finley.gmair.util.Encryption;
 import finley.gmair.util.ResponseCode;
@@ -46,6 +49,23 @@ public class AdminAuthController {
         }
         result.setResponseCode(ResponseCode.RESPONSE_ERROR);
         result.setDescription("Please make sure you fill the username and password");
+        return result;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/admin/create")
+    public ResultData create(AdminForm form) {
+        ResultData result = new ResultData();
+        if (!StringUtils.isEmpty(form.getEmail()) && !StringUtils.isEmpty(form.getName()) && !StringUtils.isEmpty(form.getPassword())) {
+            Admin admin = new Admin(form.getEmail(), form.getName(), Encryption.md5(form.getPassword()));
+            ResultData response = adminService.createAdmin(admin);
+            if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
+                result.setResponseCode(ResponseCode.RESPONSE_OK);
+                result.setDescription(new StringBuffer("Admin successfully created, ").append(JSON.toJSONString(response.getData())).toString());
+            } else {
+                result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                result.setDescription(new StringBuffer("Fail to create admin with info: ").append(JSON.toJSONString(admin)).toString());
+            }
+        }
         return result;
     }
 }
---
+++ common/src/main/java/finley/gmair/form/admin/AdminForm.java
@@ -0,0 +1,37 @@
+package finley.gmair.form.admin;
+
+public class AdminForm {
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    public AdminForm() {
+        super();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
