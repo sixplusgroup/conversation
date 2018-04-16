ade2f6f3d54935bbb53672e0a6028865ab5cabf6
xufan.zhang <sunshinezxf@hotmail.com>
2018-04-16 21:58:35 +0800

    modification

---
+++ installation/src/main/java/finley/gmair/installation/InstallationClient.java
@@ -0,0 +1,11 @@
+package finley.gmair.installation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallationClient {
+    public void main(String[] args) {
+        SpringApplication.run(InstallationClient.class, args);
+    }
+}
