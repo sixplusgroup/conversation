6ae84e6c2c4f81502f4943f0631fd6e422b75e5f
xufan.zhang <sunshinezxf@hotmail.com>
2018-04-21 16:40:23 +0800

    modify express table

--- common/src/main/java/finley/gmair/model/express/ExpressStatus.java
+++ common/src/main/java/finley/gmair/model/express/ExpressStatus.java
@@ -1,4 +1,26 @@
 package finley.gmair.model.express;
 
-public class ExpressStatus {
+import finley.gmair.model.EnumValue;
+
+public enum ExpressStatus implements EnumValue {
+    ASSIGNED(0), PICKED(1), SHIPPING(2), RECEIVED(3), RETURNED(4);
+
+    private static ExpressStatus[] list = new ExpressStatus[]{ASSIGNED, PICKED, SHIPPING, RECEIVED, RETURNED};
+
+    int value;
+
+    ExpressStatus(int value) {
+        this.value = value;
+    }
+
+    public static ExpressStatus fromValue(int value) {
+        if (value < 0 || value > list.length) {
+            return null;
+        }
+        return list[value];
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
