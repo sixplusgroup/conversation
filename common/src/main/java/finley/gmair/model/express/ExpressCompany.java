7f0439203ba9996cbdc1eadebf22be6b7d0c155e
xufan.zhang <sunshinezxf@hotmail.com>
2018-04-21 15:37:35 +0800

    fix missing attrs in express company

--- common/src/main/java/finley/gmair/model/express/ExpressCompany.java
+++ common/src/main/java/finley/gmair/model/express/ExpressCompany.java
@@ -1,4 +1,35 @@
 package finley.gmair.model.express;
 
-public class ExpressCompany {
+import finley.gmair.model.Entity;
+
+public class ExpressCompany extends Entity {
+
+    private String companyId;
+
+    private String companyName;
+
+    public ExpressCompany() {
+        super();
+    }
+
+    public ExpressCompany(String companyName) {
+        this();
+        this.companyName = companyName;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
---
+++ express/update.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `gmair_express`.`express_company`
+  ADD COLUMN `company_code` VARCHAR(45) NOT NULL
+  AFTER `company_name`;
