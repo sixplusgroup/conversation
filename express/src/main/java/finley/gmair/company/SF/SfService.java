6df8a4bb1ca58c7c20775002507e73537cbc8e88
xwz <982298763@qq.com>
2018-04-23 17:05:47 +0800

    store sf id and key

---
+++ express/src/main/java/finley/gmair/company/SF/SfService.java
@@ -0,0 +1,10 @@
+package finley.gmair.company.SF;
+
+public class SfService {
+
+    private final static String appId = "00047409";
+
+    private final static String appKey = "F6CF4BE11B81AA69CC96B8A39FF75F0F";
+
+
+}
