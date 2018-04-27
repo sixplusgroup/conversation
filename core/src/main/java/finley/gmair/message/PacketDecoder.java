343ad4545f0da1e3114a07a9f87e6899c0e2968a
xufan.zhang <sunshinezxf@hotmail.com>
2018-04-27 10:50:18 +0800

    modify express column

---
+++ core/src/main/java/finley/gmair/message/PacketDecoder.java
@@ -0,0 +1,4 @@
+package finley.gmair.message;
+
+public class PacketDecoder {
+}
--- express/update.sql
+++ express/update.sql
@@ -4,5 +4,13 @@ ALTER TABLE `gmair_express`.`express_company`
   AFTER `company_name`;
 
 ALTER TABLE `gmair_express`.`parcel_express`
-  CHANGE COLUMN `code_value` `code_value` VARCHAR(45) NULL ,
-  ADD COLUMN `parcel_type` TINYINT(1) NOT NULL DEFAULT 0 AFTER `express_status`;
+  CHANGE COLUMN `code_value` `code_value` VARCHAR(45) NULL,
+  ADD COLUMN `parcel_type` TINYINT(1) NOT NULL DEFAULT 0
+  AFTER `express_status`;
+
+#2018.04.27
+ALTER TABLE `gmair_express`.`order_express`
+  ADD COLUMN `deliver_time` DATETIME NULL
+  AFTER `express_status`,
+  ADD COLUMN `receive_time` DATETIME NULL
+  AFTER `deliver_time`;
