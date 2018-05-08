84be735723b95296268bdb794d6a4ed59355810d
xufan.zhang <sunshinezxf@hotmail.com>
2018-05-08 10:57:44 +0800

    add rest controller annotation

--- install-mp/src/main/java/finley/gmair/controller/MemberController.java
+++ install-mp/src/main/java/finley/gmair/controller/MemberController.java
@@ -8,25 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/install-mp/member")
 public class MemberController {
     @Autowired
     private MemberService memberService;
 
     //工人第一次登录时触发,将微信和手机号绑定
-    @RequestMapping(method = RequestMethod.POST, value="/setwechat")
+    @RequestMapping(method = RequestMethod.POST, value = "/setwechat")
     public ResultData setwechat(String memberPhone, String wechatId) {
         ResultData result = new ResultData();
 
-        memberPhone=memberPhone.trim();
-        wechatId=wechatId.trim();
+        memberPhone = memberPhone.trim();
+        wechatId = wechatId.trim();
 
         //check empty input
-        if(StringUtils.isEmpty(memberPhone) || StringUtils.isEmpty(wechatId)){
+        if (StringUtils.isEmpty(memberPhone) || StringUtils.isEmpty(wechatId)) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription("Please provide the all information");
             return result;
@@ -35,18 +38,16 @@ public class MemberController {
         //according to the memberPhone,find the member
         Member member = new Member();
         Map<String, Object> condition = new HashMap<>();
-        condition.put("memberPhone",memberPhone);
-        condition.put("blockFlag",false);
+        condition.put("memberPhone", memberPhone);
+        condition.put("blockFlag", false);
         ResultData response = memberService.fetchMember(condition);
-        if(response.getResponseCode() == ResponseCode.RESPONSE_OK){
-            member = ((List<Member>)response.getData()).get(0);
-        }
-        else if(response.getResponseCode() == ResponseCode.RESPONSE_NULL){
+        if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            member = ((List<Member>) response.getData()).get(0);
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_NULL) {
             result.setDescription("can not find the member with memberPhone");
             result.setResponseCode(ResponseCode.RESPONSE_NULL);
             return result;
-        }
-        else if(response.getResponseCode() == ResponseCode.RESPONSE_ERROR){
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription("server is busy now,please try again later");
             return result;
@@ -55,12 +56,11 @@ public class MemberController {
         //update the member
         member.setWechatId(wechatId);
         response = memberService.updateMember(member);
-        if(response.getResponseCode() == ResponseCode.RESPONSE_OK){
+        if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
             result.setResponseCode(ResponseCode.RESPONSE_OK);
             result.setData(member);
             result.setDescription("success to update the member");
-        }
-        else if(response.getResponseCode() == ResponseCode.RESPONSE_ERROR){
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription("server is busy now,please try again later");
         }
@@ -70,25 +70,19 @@ public class MemberController {
 
 
     @RequestMapping(method = RequestMethod.GET, value = "/findphone")
-    public ResultData findPhone(String memberPhone)
-    {
+    public ResultData findPhone(String memberPhone) {
         ResultData result = new ResultData();
-        Map<String,Object> condition = new HashMap<>();
-        condition.put("memberPhone",memberPhone);
-        condition.put("blockFlag",false);
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("memberPhone", memberPhone);
+        condition.put("blockFlag", false);
         ResultData response = memberService.fetchMember(condition);
-        if(response.getResponseCode()==ResponseCode.RESPONSE_ERROR)
-        {
+        if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription("Fail to get member info");
-        }
-        else if(response.getResponseCode()==ResponseCode.RESPONSE_NULL)
-        {
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_NULL) {
             result.setResponseCode(ResponseCode.RESPONSE_NULL);
             result.setDescription("No member info at the moment");
-        }
-        else
-        {
+        } else {
             result.setResponseCode(ResponseCode.RESPONSE_OK);
             result.setData(response.getData());
             result.setDescription("Success to get member info");
@@ -97,24 +91,19 @@ public class MemberController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/findwechat")
-    public ResultData findwechat(String wechatId){
+    public ResultData findwechat(String wechatId) {
         ResultData result = new ResultData();
-        Map<String,Object> condition = new HashMap<>();
-        condition.put("wechatId",wechatId);
-        condition.put("blockFlag",false);
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("wechatId", wechatId);
+        condition.put("blockFlag", false);
         ResultData response = memberService.fetchMember(condition);
-        if(response.getResponseCode()==ResponseCode.RESPONSE_ERROR)
-        {
+        if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription("Fail to get member info.");
-        }
-        else if(response.getResponseCode()==ResponseCode.RESPONSE_NULL)
-        {
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_NULL) {
             result.setResponseCode(ResponseCode.RESPONSE_NULL);
             result.setDescription("No member info at the moment.");
-        }
-        else
-        {
+        } else {
             result.setResponseCode(ResponseCode.RESPONSE_OK);
             result.setData(response.getData());
             result.setDescription("Success to get member info.");
