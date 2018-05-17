3f73c36f25a2a4f15e676860de5fc8190dc47db0
u1095279176893 <1095279176893@tianchi.com>
2018-05-17 17:37:58 +0800

    machine quality

--- machine/src/main/java/finley/gmair/controller/MachineAirQualityController.java
+++ machine/src/main/java/finley/gmair/controller/MachineAirQualityController.java
@@ -2,7 +2,9 @@ package finley.gmair.controller;
 
 
 import finley.gmair.form.air.MachineAirQualityForm;
+import finley.gmair.model.air.MachineAirQuality;
 import finley.gmair.service.MachineAirQualityService;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,33 @@ public class MachineAirQualityController {
     @Autowired
     private MachineAirQualityService machineAirQualityService;
 
-    @RequestMapping(value = "/finley/gmair/machine/create", method = RequestMethod.POST)
+    @RequestMapping(value = "/machine/create", method = RequestMethod.POST)
     private ResultData createMachineAirQuality(MachineAirQualityForm form) {
         ResultData result = new ResultData();
+
+        if (form.getQrcode() == null) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription("parameter qrcode is required");
+            return result;
+        }
+
+        MachineAirQuality machineAirQuality = new MachineAirQuality();
+        machineAirQuality.setQrcode(form.getQrcode());
+        machineAirQuality.setPm25(form.getPm25());
+        machineAirQuality.setHumidity(form.getHumidity());
+        machineAirQuality.setTemperature(form.getTemperature());
+        machineAirQuality.setCo2(form.getCo2());
+
+        ResultData response = machineAirQualityService.add(machineAirQuality);
+
+        if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription("服务器忙，请稍后再试");
+        } else if (response.getResponseCode() == ResponseCode.RESPONSE_NULL){
+            result.setResponseCode(ResponseCode.RESPONSE_NULL);
+        } else {
+            result.setData(response.getData());
+        }
 
         return result;
     }
