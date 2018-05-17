222dc8ac847a246f346272fe3679356af0367776
xufan.zhang <sunshinezxf@hotmail.com>
2018-05-17 16:49:25 +0800

    optimize log module

--- log/src/main/java/finley/gmair/dao/BaseDao.java
+++ log/src/main/java/finley/gmair/dao/BaseDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import javax.annotation.Resource;
 
 /**
- * 该类用于管理持久层与数据库的连接monge template, 所有的Dao需要继承此方法
+ * 该类用于管理持久层与数据库的连接mongo template, 所有的Dao需要继承此方法
  * Created by xwz on 5/17/18.
  */
 public class BaseDao {
