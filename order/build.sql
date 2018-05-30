d2db714ade77269c5a98c9c13e9f93a2f3cffe39
xufan.zhang <sunshinezxf@hotmail.com>
2018-05-30 23:30:36 +0800

    modify sql structure

--- order/build.sql
+++ order/build.sql
@@ -1,59 +1,127 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 118.31.78.254    Database: gmair_order
+-- ------------------------------------------------------
+-- Server version	5.5.58-0+deb8u1
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema gmair_order
--- -----------------------------------------------------
+create database gmair_order;
 
--- -----------------------------------------------------
--- Schema gmair_order
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `gmair_order` DEFAULT CHARACTER SET utf8 ;
-USE `gmair_order` ;
+use gmair_order;
 
--- -----------------------------------------------------
--- Table `gmair_order`.`platform_order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `gmair_order`.`platform_order` (
+--
+-- Table structure for table `order_commodity`
+--
+
+DROP TABLE IF EXISTS `order_commodity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_commodity` (
+  `com_id` VARCHAR(20) NOT NULL,
+  `com_name` VARCHAR(45) DEFAULT NULL,
+  `com_type` TINYINT(1) NOT NULL DEFAULT '0',
+  `com_price` DOUBLE NOT NULL DEFAULT '0',
+  `block_flag` TINYINT(1) NOT NULL DEFAULT '0',
+  `create_time` DATETIME NOT NULL,
+  PRIMARY KEY (`com_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_item`
+--
+
+DROP TABLE IF EXISTS `order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_item` (
+  `item_id` VARCHAR(20) NOT NULL,
   `order_id` VARCHAR(20) NOT NULL,
-  `order_no` VARCHAR(50) NULL,
+  `item_name` VARCHAR(63) NOT NULL,
+  `item_price` DECIMAL(10 , 0 ) NOT NULL,
+  `quantity` INT(11) NOT NULL DEFAULT '1',
+  `block_flag` TINYINT(1) NOT NULL DEFAULT '0',
+  `create_time` DATETIME NOT NULL,
+  PRIMARY KEY (`item_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_location_retry_count`
+--
+
+DROP TABLE IF EXISTS `order_location_retry_count`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_location_retry_count` (
+  `order_id` VARCHAR(20) NOT NULL,
+  `retry_count` INT(11) NOT NULL DEFAULT '1',
+  `update_time` DATETIME NOT NULL,
+  `block_flag` TINYINT(1) NOT NULL DEFAULT '0',
+  `create_time` DATETIME NOT NULL,
+  PRIMARY KEY (`order_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `platform_order`
+--
+
+DROP TABLE IF EXISTS `platform_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `platform_order` (
+  `order_id` VARCHAR(20) NOT NULL,
+  `order_no` VARCHAR(50) DEFAULT NULL,
   `consignee` VARCHAR(45) NOT NULL,
   `phone` VARCHAR(45) NOT NULL,
   `address` VARCHAR(100) NOT NULL,
-  `province` VARCHAR(45) NOT NULL,
-  `city` VARCHAR(45) NOT NULL,
-  `district` VARCHAR(45) NULL,
-  `total_price` DOUBLE NOT NULL DEFAULT 0,
-  `block_flag` TINYINT(1) NOT NULL DEFAULT 0,
+  `province` VARCHAR(45) DEFAULT NULL,
+  `city` VARCHAR(45) DEFAULT NULL,
+  `district` VARCHAR(45) DEFAULT NULL,
+  `total_price` DOUBLE NOT NULL DEFAULT '0',
+  `order_channel` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(100) DEFAULT NULL,
+  `order_status` TINYINT(1) NOT NULL DEFAULT '0',
+  `block_flag` TINYINT(1) NOT NULL DEFAULT '0',
   `create_time` DATETIME NOT NULL,
-  PRIMARY KEY (`order_id`))
-  ENGINE = InnoDB;
+  PRIMARY KEY (`order_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `platform_order_channel`
+--
 
--- -----------------------------------------------------
--- Table `gmair_order`.`order_item`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `gmair_order`.`order_item` (
-  `item_id` VARCHAR(20) NOT NULL,
-  `order_id` VARCHAR(20) NOT NULL,
-  `item_name` VARCHAR(50) NOT NULL,
-  `quantity` DOUBLE NOT NULL DEFAULT 1,
-  `item_price` DOUBLE NOT NULL DEFAULT 0,
-  `block_flag` TINYINT(1) NOT NULL DEFAULT 0,
+DROP TABLE IF EXISTS `platform_order_channel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `platform_order_channel` (
+  `channel_id` VARCHAR(20) NOT NULL,
+  `channel_name` VARCHAR(45) NOT NULL,
+  `block_flag` TINYINT(1) NOT NULL DEFAULT '0',
   `create_time` DATETIME NOT NULL,
-  PRIMARY KEY (`item_id`),
-  INDEX `fk_order_item_platform_order1_idx` (`order_id` ASC),
-  CONSTRAINT `fk_order_item_platform_order1`
-  FOREIGN KEY (`order_id`)
-  REFERENCES `gmair_order`.`platform_order` (`order_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-  ENGINE = InnoDB;
+  PRIMARY KEY (`channel_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2018-05-30 21:17:12
