618e5e344aee7ec28cfcb3e798bd2506e88664cc
xufan.zhang <sunshinezxf@hotmail.com>
2018-05-31 16:45:54 +0800

    add interface 4 order

--- management/src/main/java/finley/gmair/controller/ExpressController.java
+++ management/src/main/java/finley/gmair/controller/ExpressController.java
@@ -1,4 +1,27 @@
 package finley.gmair.controller;
 
+import finley.gmair.service.ExpressService;
+import finley.gmair.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management/express")
 public class ExpressController {
+
+    @Autowired
+    private ExpressService expressService;
+
+    @GetMapping("/company/query")
+    public ResultData companyFetch() {
+        return expressService.companyQuery();
+    }
+
+    @GetMapping("/order/query/{orderId}")
+    public ResultData orderFetch(@PathVariable("orderId") String orderId) {
+        return expressService.orderQuery(orderId);
+    }
 }
--- management/src/main/java/finley/gmair/controller/OrderController.java
+++ management/src/main/java/finley/gmair/controller/OrderController.java
@@ -1,26 +1,53 @@
 package finley.gmair.controller;
 
+import finley.gmair.service.OrderFormService;
 import finley.gmair.service.OrderService;
 import finley.gmair.util.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/management/order")
 public class OrderController {
     @Autowired
+    private OrderFormService orderFormService;
+
+    @Autowired
     private OrderService orderService;
 
-
-    @CrossOrigin
     @RequestMapping(method = RequestMethod.POST, value = "/upload")
     public ResultData upload(MultipartHttpServletRequest request) {
-        return orderService.upload(request.getFile("order_list"));
+        return orderFormService.upload(request.getFile("order_list"));
+    }
+
+    @GetMapping("/channel/list")
+    public ResultData channelList() {
+        return orderService.channelList();
+    }
+
+    @GetMapping("/list")
+    public ResultData orderList() {
+        return orderService.orderList();
+    }
+
+    @PostMapping("/create")
+    public ResultData orderCreate(String order) {
+        return orderService.orderCreate(order);
+    }
+
+    @GetMapping("/{orderId}/info")
+    public ResultData orderInfo(@PathVariable("orderId") String orderId) {
+        return orderService.orderInfo(orderId);
+    }
+
+    @GetMapping("/commodity/list")
+    public ResultData commodityList() {
+        return orderService.commodityList();
+    }
+
+    @PostMapping("/order/deliver")
+    public ResultData orderDeliver(String orderId, String company, String expressNo) {
+        return orderService.orderDeliver(orderId, company, expressNo);
     }
 }
