a3fb7b259a32e11672a07c18d7b7ec93e662014c
xufan.zhang <sunshinezxf@hotmail.com>
2018-05-31 12:24:48 +0800

    add interface for members

--- management/src/main/java/finley/gmair/service/InstallService.java
+++ management/src/main/java/finley/gmair/service/InstallService.java
@@ -3,10 +3,21 @@ package finley.gmair.service;
 import finley.gmair.util.ResultData;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "install-agent")
 public interface InstallService {
 
     @GetMapping("/installation/team/list")
     ResultData fetchTeamList();
+
+    @PostMapping("/installation/team/create")
+    ResultData createTeam(@RequestParam("teamName") String teamName, @RequestParam("teamArea") String teamArea, @RequestParam("teamDescription") String teamDescription);
+
+    @GetMapping("/installation/member/list")
+    ResultData fetchMemberList(@RequestParam("teamId") String teamId);
+
+    @PostMapping("/installation/member/create")
+    ResultData createMember(@RequestParam("teamId") String teamId, @RequestParam("memberPhone") String memberPhone, @RequestParam("memberName") String memberName, @RequestParam("memberRole") int memberRole);
 }
