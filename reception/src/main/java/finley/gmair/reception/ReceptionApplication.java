a2bc1a5948218a8d690ceef5caa88b81829efa98
xufan.zhang <sunshinezxf@hotmail.com>
2018-06-01 08:51:24 +0800

    init structure for reception

---
+++ reception/src/main/java/finley/gmair/reception/ReceptionApplication.java
@@ -0,0 +1,12 @@
+package finley.gmair.reception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ReceptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionApplication.class, args);
+    }
+}
