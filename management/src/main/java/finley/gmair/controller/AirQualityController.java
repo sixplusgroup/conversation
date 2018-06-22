44e5baf3a04a766aa3d5574e2a465f022c8e4181
xxh13 <1095279176893@tianchi.com>
2018-06-22 16:26:10 +0800

    add airquality interface in management module

--- management/src/main/java/finley/gmair/controller/AirQualityController.java
+++ management/src/main/java/finley/gmair/controller/AirQualityController.java
@@ -4,6 +4,7 @@ import finley.gmair.service.AirQualityService;
 import finley.gmair.util.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,33 @@ public class AirQualityController {
     @Autowired
     private AirQualityService airQualityService;
 
-    @GetMapping(value = "/hourly/cityAqi")
+    @GetMapping("/hourly/cityAqi")
     public ResultData allHourlyCityAqi() {
         return airQualityService.allHourlyCityAqi();
+    }
+
+    @GetMapping("/latest")
+    ResultData getLatestCityAirQuality(){
+        return airQualityService.getLatestCityAirQuality();
+    }
+
+    @GetMapping("/latest/{cityId}")
+    ResultData getLatestCityAirQuality(@PathVariable String cityId){
+        return airQualityService.getLatestCityAirQuality(cityId);
+    }
+
+    @GetMapping("/station/{stationId}")
+    ResultData fetchLatestByStationId(@PathVariable String stationId){
+        return airQualityService.fetchLatestByStationId(stationId);
+    }
+
+    @GetMapping("/station/city/{cityId}")
+    ResultData fetchLatestByCityId(@PathVariable String cityId){
+        return airQualityService.fetchLatestByCityId(cityId);
+    }
+
+    @GetMapping("/station/list")
+    ResultData fetchLatestStationList(){
+        return airQualityService.fetchLatestStationList();
     }
 }
--- management/src/main/java/finley/gmair/service/AirQualityService.java
+++ management/src/main/java/finley/gmair/service/AirQualityService.java
@@ -3,6 +3,7 @@ package finley.gmair.service;
 import finley.gmair.util.ResultData;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @author shenghaohu@hshvip.com
@@ -13,4 +14,19 @@ public interface AirQualityService {
 
     @GetMapping("/airquality/hourly/cityAqi")
     ResultData allHourlyCityAqi();
+
+    @GetMapping("/airquality/latest")
+    ResultData getLatestCityAirQuality();
+
+    @GetMapping(value = "/airquality/latest/{cityId}")
+    ResultData getLatestCityAirQuality(@PathVariable String cityId);
+
+    @GetMapping("/airquality/station/{stationId}")
+    ResultData fetchLatestByStationId(@PathVariable String stationId);
+
+    @GetMapping("/airquality/station/city/{cityId}")
+    ResultData fetchLatestByCityId(@PathVariable String cityId);
+
+    @GetMapping("/airquality/station/list")
+    ResultData fetchLatestStationList();
 }
