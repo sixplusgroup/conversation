8fe9bed2c069d45e298b3aee61cc9d6bba1e2fd0
xufan.zhang <sunshinezxf@hotmail.com>
2018-06-29 21:04:20 +0800

    add interface for configuration obtain

---
+++ wechat/src/main/java/finley/gmair/util/Configuration.java
@@ -0,0 +1,71 @@
+package finley.gmair.util;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Configuration {
+    private String appId;
+    private String jsapiTicket;
+    private String nonceStr;
+    private String timestamp;
+    private String signature;
+    private String url;
+
+    public Configuration() {
+        super();
+        this.appId = WechatProperties.getValue("wechat_appid");
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getJsapiTicket() {
+        return jsapiTicket;
+    }
+
+    public void setJsapiTicket(String jsapiTicket) {
+        this.jsapiTicket = jsapiTicket;
+    }
+
+    public String getNonceStr() {
+        return nonceStr;
+    }
+
+    public void setNonceStr(String nonceStr) {
+        this.nonceStr = nonceStr;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return JSONObject.toJSONString(this);
+    }
+}
+
