b253e38ce679d5346a43452efe57253368c2ff03
hushenghao <295065492@qq.com>
2018-07-20 18:07:23 +0800

    update

--- wechat/src/main/java/finley/gmair/service/AuthConsumerService.java
+++ wechat/src/main/java/finley/gmair/service/AuthConsumerService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("auth-consumer-agent")
+@FeignClient("consumer-auth-agent")
 public interface AuthConsumerService {
 
     @PostMapping("/auth/probe/consumerid/by/openid")
