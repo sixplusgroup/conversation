4dde40ddfa115894e957562e219a64c130037e8c
hushenghao <295065492@qq.com>
2018-07-23 17:22:40 +0800

    update article message

--- wechat/src/main/java/finley/gmair/wechat/WechatApplication.java
+++ wechat/src/main/java/finley/gmair/wechat/WechatApplication.java
@@ -261,7 +261,6 @@ public class WechatApplication {
             if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
                 JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(response.getData()));
                 sb.append(bindName + "(在线)\n");
-                sb.append("-----\n");
                 sb.append("pm2.5: " + jsonObject.getIntValue("pm2_5") + "µg/m³\n");
                 sb.append("室内温度: " + jsonObject.getIntValue("temp") + "℃\n");
                 sb.append("室内湿度: " + jsonObject.getIntValue("humid") + "%\n");
