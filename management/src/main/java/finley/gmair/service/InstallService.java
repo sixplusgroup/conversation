94c5d013ace182943f99015a44b76a2afa75d603
xufan.zhang <sunshinezxf@hotmail.com>
2018-09-18 10:37:33 +0800

    fix bugs in service name

--- management/src/main/java/finley/gmair/service/InstallService.java
+++ management/src/main/java/finley/gmair/service/InstallService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "installation-agent")
+@FeignClient(value = "install-agent")
 public interface InstallService {
 
     @GetMapping("/installation/team/list")
