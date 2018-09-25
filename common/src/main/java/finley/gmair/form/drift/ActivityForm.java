c7eb24aa27129b298a9877132bf05b99b05a7964
hushenghao <295065492@qq.com>
2018-09-25 18:04:06 +0800

    add activity interface

---
+++ common/src/main/java/finley/gmair/form/drift/ActivityForm.java
@@ -0,0 +1,66 @@
+package finley.gmair.form.drift;
+
+import java.util.Date;
+
+public class ActivityForm {
+
+    private String goodsId;
+
+    private String activityName;
+
+    private int repositorySize;
+
+    private double threshold;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public int getRepositorySize() {
+        return repositorySize;
+    }
+
+    public void setRepositorySize(int repositorySize) {
+        this.repositorySize = repositorySize;
+    }
+
+    public double getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
--- drift/src/main/java/finley/gmair/controller/ActivityController.java
+++ drift/src/main/java/finley/gmair/controller/ActivityController.java
@@ -1,7 +1,23 @@
 package finley.gmair.controller;
 
+import finley.gmair.form.drift.ActivityForm;
+import finley.gmair.service.ActivityService;
+import finley.gmair.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/assemble/activity")
 public class ActivityController {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @PostMapping(value = "/create")
+    public ResultData createDriftActivity(ActivityForm form) {
+        ResultData result = new ResultData();
+        return result;
+    }
 }
