c50c20910f4e5c750d2e7ca3d8426b551a98dac6
hushenghao <295065492@qq.com>
2018-09-27 15:35:59 +0800

    add auth-consumer feign service to drift module

--- drift/src/main/java/finley/gmair/drift/DriftApplication.java
+++ drift/src/main/java/finley/gmair/drift/DriftApplication.java
@@ -2,9 +2,11 @@ package finley.gmair.drift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients({"finley.gmair.service"})
 @ComponentScan({"finley.gmair.controller", "finley.gmair.service", "finley.gmair.dao"})
 public class DriftApplication {
 
