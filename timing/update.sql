d25643f341ebf721640ed4f51a2d968d470c73d5
hushengao <295065492@qq.com>
2018-12-13 21:31:08 +0800

    update sql

--- timing/update.sql
+++ timing/update.sql
@@ -2,10 +2,9 @@
 CREATE TABLE `gmair_schedule`.`task` (
   `task_id` VARCHAR(45) NOT NULL,
   `task_name` VARCHAR(45) NOT NULL,
-  `frequent` VARCHAR(45) NOT NULL，
-  `description` VARCHAR(50) NOT NULL，
+  `frequent` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(50) NOT NULL,
   `status` TINYINT(1) NOT NULL,
   `block_flag` TINYINT(1) NOT NULL,
   `create_time` DATETIME NOT NULL,
   PRIMARY KEY (`task_id`));
-
