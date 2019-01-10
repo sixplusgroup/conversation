01546e66325411f291a84e41e9c2dc40f4f21e69
kukubao207 <502914814@qq.com>
2019-01-10 19:24:55 +0800

    delete useless import

--- data-analysis/src/main/java/finley/gmair/controller/MachineStatusController.java
+++ data-analysis/src/main/java/finley/gmair/controller/MachineStatusController.java
@@ -3,22 +3,19 @@ package finley.gmair.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import finley.gmair.model.dataAnalysis.*;
 import finley.gmair.service.*;
 import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import finley.gmair.util.TimeUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
-import java.util.*;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/data/analysis/machine/status")
