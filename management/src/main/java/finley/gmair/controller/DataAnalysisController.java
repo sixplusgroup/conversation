5050bf0afc8b3003706a84bb2b1866e0ee1b21df
kukubao207 <502914814@qq.com>
2019-01-16 16:02:04 +0800

    change management

--- management/src/main/java/finley/gmair/controller/DataAnalysisController.java
+++ management/src/main/java/finley/gmair/controller/DataAnalysisController.java
@@ -1,5 +1,6 @@
 package finley.gmair.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import finley.gmair.service.DataAnalysisAgent;
 import finley.gmair.service.MachineService;
 import finley.gmair.util.ResultData;
@@ -19,19 +20,35 @@ public class DataAnalysisController {
 
     @GetMapping("/machine/status/{statusType}/lastNday")
     public ResultData fetchLastNDayIndoorPm25(String qrcode, int lastNday, @PathVariable("statusType") String statusType) {
-        if (statusType.equals("citypm25"))
-            return machineService.getCitylastNdayData(qrcode, lastNday);
-        if (statusType.equals("outpm25"))
-            return machineService.fetchLastNDayData(qrcode, lastNday);
+        ResultData result = new ResultData();
+        JSONObject jsonObject = new JSONObject();
+        if (statusType.equals("pm25")){
+            ResultData response = machineService.getCitylastNdayData(qrcode, lastNday);
+            jsonObject.put("citypm25",response.getData());
+            response = machineService.fetchLastNDayData(qrcode,lastNday);
+            jsonObject.put("outpm25",response.getData());
+            response = dataAnalysisAgent.fetchLastNDayData(qrcode,lastNday,statusType);
+            jsonObject.put("indoorpm25",response.getData());
+            result.setData(jsonObject);
+            return result;
+        }
         return dataAnalysisAgent.fetchLastNDayData(qrcode, lastNday, statusType);
     }
 
     @GetMapping("/machine/status/{statusType}/lastNhour")
     public ResultData fetchLastNHourIndoorPm25(String qrcode, int lastNhour, @PathVariable("statusType") String statusType) {
-        if (statusType.equals("citypm25"))
-            return machineService.getCitylastNhourData(qrcode, lastNhour);
-        if (statusType.equals("outpm25"))
-            return machineService.fetchLastNHourData(qrcode, lastNhour);
+        ResultData result = new ResultData();
+        JSONObject jsonObject = new JSONObject();
+        if (statusType.equals("pm25")){
+            ResultData response = machineService.getCitylastNhourData(qrcode, lastNhour);
+            jsonObject.put("citypm25",response.getData());
+            response = machineService.fetchLastNHourData(qrcode,lastNhour);
+            jsonObject.put("outpm25",response.getData());
+            response = dataAnalysisAgent.fetchLastNHourData(qrcode,lastNhour,statusType);
+            jsonObject.put("indoorpm25",response.getData());
+            result.setData(jsonObject);
+            return result;
+        }
         return dataAnalysisAgent.fetchLastNHourData(qrcode, lastNhour, statusType);
     }
 
