97bfcefdb98ae65133572cf2293d89c79775ee38
xufan.zhang <sunshinezxf@hotmail.com>
2019-02-26 22:19:48 +0800

    add thymleaf in monitor

---
+++ installation/src/main/java/finley/gmair/controller/ModeratorController.java
@@ -0,0 +1,8 @@
+package finley.gmair.controller;
+
+/**
+ * @author fan
+ * @create_time 2019-2019/2/22 12:00 PM
+ */
+public class ModeratorController {
+}
---
+++ monitor/src/main/java/finley/gmair/controller/PageController.java
@@ -0,0 +1,23 @@
+package finley.gmair.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author fan
+ * @create_time 2019-2019/2/26 9:47 PM
+ */
+@RestController
+@RequestMapping("/machine")
+public class PageController {
+
+    @GetMapping("/tv/{qrcode}")
+    public ModelAndView viewOnTV(@PathVariable("qrcode") String qrcode) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("tv_view");
+        return view;
+    }
+}
