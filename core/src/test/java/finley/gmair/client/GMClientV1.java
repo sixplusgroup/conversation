d44d613d9d2a75c03037bfdecb6cc8ccc1b1f837
biggrass <hhga2014@163.com>
2019-03-06 15:56:57 +0800

    machine list status

--- core/src/test/java/finley/gmair/client/GMClientV1.java
+++ core/src/test/java/finley/gmair/client/GMClientV1.java
@@ -7,10 +7,10 @@ public class GMClientV1 {
     public static void main(String[] args) {
         int port = 8888;
         //服务器host: 118.31.78.254     my host: 192.168.2.59
-        for(int i=0;i<10;i++) {
-        String uid = "v1test" + i;
+//        for(int i=0;i<10;i++) {
+        String uid = "F0FE6B9BA794" ;
         GMClientV1Handler server = new GMClientV1Handler(null, port, uid,5000);
         new Thread(server, "server").start();
-        }
+//        }
     }
 }
