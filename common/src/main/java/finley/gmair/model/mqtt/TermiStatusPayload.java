1696245bd90c043160b5c8a59871171079b306fe
hushengao <295065492@qq.com>
2019-03-25 15:02:02 +0800

    update pom.xml

---
+++ common/src/main/java/finley/gmair/model/mqtt/TermiStatusPayload.java
@@ -0,0 +1,4 @@
+package finley.gmair.model.mqtt;
+
+public class TermiStatusPayload {
+}
---
+++ mqtt-core/src/main/java/finley/gmair/controller/MessageController.java
@@ -0,0 +1,9 @@
+package finley.gmair.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MessageSendController {
+}
