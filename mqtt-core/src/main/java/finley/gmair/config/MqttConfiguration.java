76a39e25a9af9422d9b3ecd257f3d22b66c30c5e
hushengao <295065492@qq.com>
2019-04-16 14:53:33 +0800

    add interfaces

--- mqtt-core/src/main/java/finley/gmair/config/MqttConfiguration.java
+++ mqtt-core/src/main/java/finley/gmair/config/MqttConfiguration.java
@@ -2,10 +2,9 @@ package finley.gmair.config;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import finley.gmair.model.mqtt.SensorPayload;
-import finley.gmair.model.mqtt.StatusPayload;
-import finley.gmair.model.mqtt.SurplusPayload;
-import finley.gmair.model.mqtt.Topic;
+import finley.gmair.controller.MachineController;
+import finley.gmair.model.mqtt.*;
+import finley.gmair.service.LogService;
 import finley.gmair.service.TopicService;
 import finley.gmair.util.MqttProperties;
 import finley.gmair.util.ResponseCode;
@@ -25,6 +24,7 @@ import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,6 +38,14 @@ public class MqttConfiguration {
 
     @Autowired
     private TopicService topicService;
+
+    @Autowired
+    private MachineController machineController;
+
+    @Autowired
+    private LogService logService;
+
+    private final static String ip = "116.62.233.170";
 
     @Value("${username}")
     private String username;
@@ -141,29 +149,100 @@ public class MqttConfiguration {
     private void handle(String topic, JSONObject json) {
         //将topic根据"/"切分为string数组，方便处理
         String[] array = topic.split("/");
-
         //根据定义的topic格式，获取message对应的machineId
         String machineId = array[2];
-
+        String base_action = array[5];
+        //对于长度为7的topic，只有上传单项传感器数据和警报处理
         if (array.length == 7) {
             //获取传感器数值类型（pm2.5, co2, temp, temp_out, humidity）其中之一
-            String sensor = array[6];
-            int value = json.getIntValue("value");
-            //todo something
+            String detail_action = array[6];
+            if (base_action.equals("sensor")) {
+                int value = json.getIntValue("value");
+                dealSingleSensor(detail_action, value);
+            }
+            if (base_action.equals("alert")) {
+                AlertPayload payload = new AlertPayload(machineId, json);
+                dealAlertMessage(detail_action, payload);
+            }
         } else {
-            String action = array[5];
-            if (action.equals("sys_status")) {
+            if (base_action.equals("sys_status")) {
                 StatusPayload payload = new StatusPayload(machineId, json);
                 //todo something
             }
-            if (action.equals("sys_surplus")) {
+            if (base_action.equals("sys_surplus")) {
                 SurplusPayload payload = new SurplusPayload(machineId, json);
                 //todo something
             }
-            if (action.equals("sensor")) {
+            if (base_action.equals("sensor")) {
                 SensorPayload payload = new SensorPayload(machineId, json);
                 //todo something
             }
+            if (base_action.equals("ack")) {
+                AckPayload payload = new AckPayload(machineId, json);
+                if (payload.getCode() != 0) {
+                    payload.setError(json.getString("error"));
+                }
+                dealAckMessage(payload);
+            }
+        }
+    }
+
+    /**
+     * 处理警报消息publish
+     * */
+    private void dealAlertMessage(String detail_action, AlertPayload payload) {
+        int code = payload.getCode();
+        String machineId = payload.getMachineId();
+        String msg = payload.getMsg();
+        if (detail_action.equals("pub")) {
+            new Thread(() -> {
+                machineController.createAlert(machineId, code, msg);
+            }).start();
+        }
+        if (detail_action.equals("rm")) {
+            new Thread(() -> {
+                machineController.updateAlert(machineId, code);
+            }).start();
+        }
+    }
+
+    /**
+     * 处理单个传感器数据消息publish
+     * */
+    private void dealSingleSensor(String detail_action, int value) {
+        if (detail_action.equals("pm2.5")) {
+
+        }
+        if (detail_action.equals("co2")) {
+
+        }
+        if (detail_action.equals("temp")) {
+
+        }
+        if (detail_action.equals("temp_out")) {
+
+        }
+        if (detail_action.equals("humidity")) {
+
+        }
+    }
+
+    /**
+     * 处理ack确认消息publish
+     * */
+    private void dealAckMessage(AckPayload payload) {
+        if (payload.getCode() == 0) {
+            new Thread(() -> {
+                logService.createMqttAckLog(payload.getAckId(), payload.getMachineId(),
+                        payload.getCode(), "Mqtt", ip, new StringBuffer("The machine: ")
+                                .append(payload.getMachineId()).append("operate the command successfully").toString());
+            }).start();
+        }
+        if (payload.getCode() != 0) {
+            new Thread(() -> {
+                logService.createMqttAckLog(payload.getAckId(), payload.getMachineId(),
+                        payload.getCode(), "Mqtt", ip, payload.getError());
+            }).start();
         }
     }
 }
