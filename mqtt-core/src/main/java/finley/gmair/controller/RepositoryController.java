f74e847a7bea40a848716d831598b33ffb81a1e1
xufan.zhang <sunshinezxf@hotmail.com>
2019-07-05 12:34:57 +0800

    add implementation for online check in mqtt core

--- mqtt-core/src/main/java/finley/gmair/controller/RepositoryController.java
+++ mqtt-core/src/main/java/finley/gmair/controller/RepositoryController.java
@@ -1,6 +1,13 @@
 package finley.gmair.controller;
 
+import finley.gmair.service.RedisService;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,11 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/6/13 11:30 AM
  */
 @RestController
+@RequestMapping("/core/repo")
 public class RepositoryController {
-    
-    public ResultData isOnline() {
-        ResultData result = new ResultData();
 
+    @Autowired
+    private RedisService redisService;
+
+    @GetMapping("/{machineId}/online")
+    public ResultData isOnline(@PathVariable("machineId") String machineId) {
+        ResultData result = new ResultData();
+        if (StringUtils.isEmpty(machineId)) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription("请提供设备的MAC地址");
+            return result;
+        }
+        if (!redisService.exists(machineId)) {
+            result.setResponseCode(ResponseCode.RESPONSE_NULL);
+            result.setDescription("当前尚未发现该设备");
+            return result;
+        }
+        result.setResponseCode(ResponseCode.RESPONSE_OK);
+        result.setDescription("该设备目前已联网");
         return result;
     }
 }
