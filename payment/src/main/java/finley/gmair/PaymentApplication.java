58b0f6cb2d7be5075da2b79eeb2f38922899c1dc
xufan.zhang <sunshinezxf@hotmail.com>
2019-07-23 15:06:57 +0800

    add new module payment

---
+++ payment/src/main/java/finley/gmair/PaymentApplication.java
@@ -0,0 +1,18 @@
+package finley.gmair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: PaymentApplication
+ * @Description: TODO
+ * @Author fan
+ * @Date 2019/7/23 3:03 PM
+ */
+
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
