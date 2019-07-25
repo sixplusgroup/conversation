8b16a12f36eb5c30e2a325338e6bd384004ccb76
xufan.zhang <sunshinezxf@hotmail.com>
2019-07-25 15:35:49 +0800

    fix bugs: add usersession table

--- drift/update.sql
+++ drift/update.sql
@@ -123,5 +123,15 @@ ALTER TABLE `gmair_drift`.`drift_activity`
   DROP COLUMN `goods_id`,
   DROP INDEX `fk_drift_activity_drift_goods1_idx`;
 
+## 2019-07-25
+
+CREATE TABLE `gmair_install`.`user_session` (
+  `open_id`     VARCHAR(100) NOT NULL,
+  `session_key` VARCHAR(100) NOT NULL,
+  `block_flag`  TINYINT(1)   NOT NULL,
+  `create_time` DATETIME     NOT NULL,
+  PRIMARY KEY (`open_id`)
+);
+
 
 
