dc55c9014460dc28c0ba173f21483b6f98d97016
lycheeshell <lycheeshell@163.com>
2019-08-30 12:47:31 +0800

    modify annotation

--- verify/src/main/java/finley/gmair/verify/VerifyApplication.java
+++ verify/src/main/java/finley/gmair/verify/VerifyApplication.java
@@ -2,9 +2,7 @@ package finley.gmair.verify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
