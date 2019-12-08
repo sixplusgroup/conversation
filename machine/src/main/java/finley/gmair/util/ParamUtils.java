92636856f2c28a2d64abbd2f67f6f908d31ac8cc
xufan.zhang <sunshinezxf@hotmail.com>
2019-12-08 22:22:42 +0800

    optimization in machine module

---
+++ machine/src/main/java/finley/gmair/util/ParamUtils.java
@@ -0,0 +1,18 @@
+package finley.gmair.util;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @ClassName: ParamUtils
+ * @Description: TODO
+ * @Author fan
+ * @Date 2019/12/8 10:12 PM
+ */
+public class ParamUtils {
+    public static boolean containEmpty(Object... args) {
+        for (Object s : args) {
+            if (StringUtils.isEmpty(s)) return false;
+        }
+        return true;
+    }
+}
