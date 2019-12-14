79af07c22226c96defd574fd1c773ef77709e7d0
jindon1020 <1132430363@qq.com>
2019-12-14 22:35:53 +0800

    add request header

--- common/src/main/java/finley/gmair/util/HttpDeal.java
+++ common/src/main/java/finley/gmair/util/HttpDeal.java
@@ -1,7 +1,6 @@
 package finley.gmair.util;
 
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,54 +14,30 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
-import sun.applet.resources.MsgAppletViewer;
 
 import java.io.IOException;
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.Map;
-import java.util.UUID;
 
 public class HttpDeal {
-//    public static String getResponse(String url) {
-//        try {
-//            HttpClient httpClient = HttpClients.createDefault();
-//            HttpGet get = new HttpGet(new URI(url));
-//            HttpResponse response = httpClient.execute(get);
-//            String userJson = EntityUtils.toString(response.getEntity());
-//            return userJson;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-
-    public static String getResponse(String url, Map<String, String> header){
-        try{
+    public static String getResponse(String url, Map<String, String> header) {
+        try {
             HttpClient httpClient = HttpClients.createDefault();
-            String token = header.get("token");
-            String password = header.get("password");
-            Long timeValue = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
-            String timeStamp = timeValue.toString();
-            System.out.println(url);
-            HttpGet httpGet = new HttpGet(url);
-            String nonce = UUID.randomUUID().toString();
-            String signature = Encryption.md5(password + "\n" + timeStamp + "\n" +  nonce+ "\n" + header.get("subUrl"));
-            System.out.println(signature);
-            httpGet.setHeader("X-AC-Token", token);
-            httpGet.setHeader("X-Date", timeStamp);
-            httpGet.setHeader("X-AC-Nonce", nonce);
-            httpGet.setHeader("X-AC-Signature", signature);
-            HttpResponse response = httpClient.execute(httpGet);
+            HttpGet get = new HttpGet(new URI(url));
+            if(header.size() > 0){
+                for (String key : header.keySet()) {
+                    get.setHeader(key, header.get(key));
+                }
+            }
+            HttpResponse response = httpClient.execute(get);
             String userJson = EntityUtils.toString(response.getEntity());
             return userJson;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
 
     public static String postJSONResponse(String url, JSONObject param, Map<String, String> header) {
         try {
