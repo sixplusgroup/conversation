4f3c8e023e04bca3a08ec70c8dc6a0c44aa620a8
ck <578045968@qq.com>
2020-07-04 19:52:31 +0800

    dao

--- machine/src/main/java/finley/gmair/dao/impl/MachineFilterCleanDaoImpl.java
+++ machine/src/main/java/finley/gmair/dao/impl/MachineFilterCleanDaoImpl.java
@@ -2,8 +2,11 @@ package finley.gmair.dao.impl;
 
 import finley.gmair.dao.BaseDao;
 import finley.gmair.dao.MachineFilterCleanDao;
+import finley.gmair.model.machine.MachineFilterClean;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
@@ -17,11 +20,32 @@ import java.util.Map;
 public class MachineFilterCleanDaoImpl extends BaseDao implements MachineFilterCleanDao {
     @Override
     public ResultData query(Map<String, Object> condition) {
-        return null;
+        ResultData result = new ResultData();
+        String qrcode=(String)condition.get("qrcode");
+        try {
+            MachineFilterClean machineFilterClean = sqlSession.selectOne("gmair.machine.machine_filter_clean.query", qrcode);
+            if (StringUtils.isEmpty(machineFilterClean)) {
+                result.setResponseCode(ResponseCode.RESPONSE_NULL);
+            }
+            result.setData(machineFilterClean);
+        } catch (Exception e) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        }
+        return result;
     }
 
     @Override
     public ResultData update(Map<String, Object> condition) {
-        return null;
+        ResultData result = new ResultData();
+        try {
+            sqlSession.update("gmair.machine.machine_filter_clean.update", condition);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setData(condition);
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        }
+        return result;
     }
 }
