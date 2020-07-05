8e393055d99762ded9fb6fec595a8c6ce59d9c16
chenguangming <980231201@qq.com>
2020-07-05 15:25:01 +0800

    optimize code structure in module Machine.

--- machine/src/main/java/finley/gmair/service/impl/MachineFilterCleanServiceImpl.java
+++ machine/src/main/java/finley/gmair/service/impl/MachineFilterCleanServiceImpl.java
@@ -1,11 +1,15 @@
 package finley.gmair.service.impl;
 
 import finley.gmair.dao.MachineFilterCleanDao;
+import finley.gmair.model.machine.MachineFilterClean;
 import finley.gmair.service.MachineFilterCleanService;
+import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,6 +21,8 @@ import java.util.Map;
 @Service
 public class MachineFilterCleanServiceImpl implements MachineFilterCleanService {
 
+    private static final int CLEAN_TIME_INTERVAL = 30;
+
     @Autowired
     private MachineFilterCleanDao machineFilterCleanDao;
 
@@ -26,7 +32,48 @@ public class MachineFilterCleanServiceImpl implements MachineFilterCleanService 
     }
 
     @Override
+    public ResultData fetchByQRCode(String qrcode) {
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("qrcode", qrcode);
+        condition.put("blockFlag", false);
+        return fetch(condition);
+    }
+
+    @Override
     public ResultData modify(Map<String, Object> condition) {
         return machineFilterCleanDao.update(condition);
+    }
+
+    @Override
+    public ResultData filterCleanCheck(MachineFilterClean selectedOne) {
+        ResultData res = new ResultData();
+        Map<String, Object> resData = new HashMap<>();
+        resData.put("qrcode", selectedOne.getQrcode());
+
+        if (selectedOne.isNeedClean()) {
+            resData.put("isNeedClean", selectedOne.isNeedClean());
+        }
+        else {
+            long dayDiff = (new Date().getTime() - selectedOne.getLastConfirmTime().getTime()) /
+                    (1000 * 60 * 60 * 24);
+            if (dayDiff >= CLEAN_TIME_INTERVAL) {
+                resData.put("isNeedClean", true);
+                //更新isNeedClean字段
+                Map<String, Object> modification = new HashMap<>();
+                modification.put("qrcode", selectedOne.getQrcode());
+                modification.put("isNeedClean", true);
+                ResultData modifyRes = modify(modification);
+                if (modifyRes.getResponseCode() != ResponseCode.RESPONSE_OK) {
+                    res.setResponseCode(ResponseCode.RESPONSE_ERROR);
+                    res.setDescription("modify MachineFilterClean failed");
+                    return res;
+                }
+            }
+            else {
+                resData.put("isNeedClean", false);
+            }
+        }
+        res.setData(resData);
+        return res;
     }
 }
