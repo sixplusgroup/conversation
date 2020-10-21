76f897c43213e79fc52cfcc8d4f6cd765cad302c
tsl19972007 <498656288@qq.com>
2020-10-21 15:26:23 +0800

    add test

--- order-new/src/main/java/finley/gmair/service/impl/TbOrderSyncServiceImpl.java
+++ order-new/src/main/java/finley/gmair/service/impl/TbOrderSyncServiceImpl.java
@@ -12,6 +12,8 @@ import finley.gmair.service.TbOrderSyncService;
 import finley.gmair.util.ResponseCode;
 import finley.gmair.util.ResultData;
 import org.apache.commons.lang.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,11 +34,12 @@ public class TbOrderSyncServiceImpl implements TbOrderSyncService {
             "receiver_mobile,receiver_phone,receiver_country,receiver_city,receiver_district,receiver_town,tmall_delivery,cn_service," +
             "delivery_cps,cutoff_minutes,delivery_time,collect_time,dispatch_time,sign_time,es_time,price,total_fee,payment,adjust_fee," +
             "received_payment,discount_fee,post_fee,credit_card_fee,step_paid_fee";
-
      */
     private static final String IMPORT_FIELDS = "tid";
 
     private static final Long PAGES_SIZE = 50L;
+
+    private Logger logger = LoggerFactory.getLogger(TbOrderSyncServiceImpl.class);
 
     @Autowired
     private TbOrderService tbOrderServiceImpl;
@@ -50,12 +53,13 @@ public class TbOrderSyncServiceImpl implements TbOrderSyncService {
     public ResultData fullImport() {
         ResultData resultData = new ResultData();
         List<TbUser> tbUserList = tbUserMapper.selectAll();
-        System.out.println("userListSize:"+tbUserList.size());
         for (TbUser tbUser : tbUserList) {
+            logger.info("startFullImport, tbUser:{}", tbUser.toString());
             //如果全量同步已执行，则返回
             if (tbUser.getLastUpdateTime() != null) {
                 resultData.setResponseCode(ResponseCode.RESPONSE_ERROR);
                 resultData.setDescription("fullImport has been executed");
+                logger.error("fullImport has been executed");
                 return resultData;
             }
             String sessionKey = tbUser.getSessionKey();
@@ -80,9 +84,8 @@ public class TbOrderSyncServiceImpl implements TbOrderSyncService {
                 long pageNo = 1L;
                 while (true) {
                     request.setPageNo(pageNo++);
-                    System.out.println(request.getStartCreated() + "-" + request.getEndCreated() + ";" + request.getPageNo());
+                    logger.info("startTime:{}, endTime:{}, pageNo:{}", request.getStartCreated(), request.getEndCreated(), request.getPageNo());
                     TradesSoldGetResponse response = tbAPIServiceImpl.tradesSoldGet(request, sessionKey);
-                    System.out.println(response.getBody());
                     for (Trade trade : response.getTrades()) {
                         tbOrderServiceImpl.handleTrade(trade);
                     }
@@ -105,10 +108,12 @@ public class TbOrderSyncServiceImpl implements TbOrderSyncService {
         ResultData resultData = new ResultData();
         List<TbUser> tbUserList = tbUserMapper.selectAll();
         for (TbUser tbUser : tbUserList) {
+            logger.info("startIncrementalImport, tbUser:{}", tbUser.toString());
             //如果全量同步还未执行,则返回
             if (tbUser.getLastUpdateTime() == null) {
                 resultData.setResponseCode(ResponseCode.RESPONSE_ERROR);
                 resultData.setDescription("fullImport is not executed");
+                logger.error("fullImport is not executed");
                 return resultData;
             }
             String sessionKey = tbUser.getSessionKey();
@@ -125,6 +130,7 @@ public class TbOrderSyncServiceImpl implements TbOrderSyncService {
             //分页同步
             while (true) {
                 request.setPageNo(pageNo++);
+                logger.info("startTime:{}, endTime:{}, pageNo:{}", request.getStartModified(), request.getEndModified(), request.getPageNo());
                 TradesSoldIncrementGetResponse response = tbAPIServiceImpl.tradesSoldIncrementGet(request, sessionKey);
                 for (Trade trade : response.getTrades()) {
                     tbOrderServiceImpl.handleTrade(trade);
