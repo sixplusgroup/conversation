b9700628ac322fb1b540b5328f9b5d6018128c1e
ckjn1997 <383739644@qq.com>
2020-10-24 17:21:24 +0800

    redis缓存序列化

--- auth-consumer/src/main/java/finley/gmair/config/RedisConfig.java
+++ auth-consumer/src/main/java/finley/gmair/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package finley.gmair.config;
 
+import finley.gmair.model.auth.VerificationCode;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,4 +25,18 @@ public class RedisConfig {
         rcm.setExpires(expires);
         return rcm;
     }
+
+    @Bean
+    public RedisTemplate<Object, VerificationCode> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        RedisTemplate<Object, VerificationCode> template = new RedisTemplate<Object, VerificationCode>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<VerificationCode> verificationCodeJackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<VerificationCode>(VerificationCode.class);
+        template.setDefaultSerializer(verificationCodeJackson2JsonRedisSerializer);
+        return template;
+    }
+
+
+
 }
