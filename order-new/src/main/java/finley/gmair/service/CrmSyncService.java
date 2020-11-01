90ab8630fbf4544343b709957f8b83dda718c5c3
zm <zhoumiao48@gmail.com>
2020-11-01 13:29:01 +0800

    中台 —> CRM系统的同步方法实现

--- order-new/src/main/java/finley/gmair/service/CrmSyncService.java
+++ order-new/src/main/java/finley/gmair/service/CrmSyncService.java
@@ -1,9 +1,18 @@
 package finley.gmair.service;
 
+import finley.gmair.model.ordernew.Order;
+import finley.gmair.model.ordernew.Trade;
+import finley.gmair.util.ResultData;
+
 /**
  * @author zm
  * @date 2020/10/26 0026 10:38
  * @description Crm系统订单同步Service
  **/
 public interface CrmSyncService {
+
+
+    ResultData updateOrderStatus(Trade interTrade);
+
+    ResultData createNewOrder(Trade interTrade);
 }
