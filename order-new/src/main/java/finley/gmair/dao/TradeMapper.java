3ec17015781e9e2eb5ae1efe320567cfeb818efa
zm <zhoumiao48@gmail.com>
2020-11-01 13:32:14 +0800

    添加方法：selectByTid、countByTid、updateStatusByTradeId、updateStatusByTid

--- order-new/src/main/java/finley/gmair/dao/TradeMapper.java
+++ order-new/src/main/java/finley/gmair/dao/TradeMapper.java
@@ -1,5 +1,7 @@
 package finley.gmair.dao;
+
 import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 
 import finley.gmair.model.dto.TbTradeDTO;
@@ -16,6 +18,11 @@ public interface TradeMapper {
 
     Trade selectByPrimaryKey(String tradeId);
 
+    /**
+     * 根据tid查询返回对应的Trade
+     */
+    List<Trade> selectByTid(@Param("tid") Long tid);
+
     int updateByPrimaryKeySelective(Trade record);
 
     int updateByPrimaryKey(Trade record);
@@ -24,4 +31,23 @@ public interface TradeMapper {
      * 将淘宝方的单笔交易导入Trade表中
      */
     int insertSelectiveWithTradeDTO(TbTradeDTO tradeDTO);
+
+    /**
+     * 查询该tid在库中是否存在对应的交易
+     */
+    Integer countByTid(@Param("tid") Long tid);
+
+    /**
+     * 根据Trade主键更新交易状态
+     */
+    int updateStatusByTradeId(
+            @Param("updatedStatus") String updatedStatus,
+            @Param("tradeId") String tradeId);
+
+    /**
+     * 根据tid封信交易状态
+     */
+    int updateStatusByTid(
+            @Param("updatedStatus") String updatedStatus,
+            @Param("tid") Long tid);
 }
