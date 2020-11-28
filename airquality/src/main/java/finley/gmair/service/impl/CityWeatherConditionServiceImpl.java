33095b11b217345cb31f6bf44c50b708c6ce18bc
Lhy <67047642+Lhy-op@users.noreply.github.com>
2020-11-28 16:31:06 +0800

    Update CityWeatherConditionServiceImpl.java

--- airquality/src/main/java/finley/gmair/service/impl/CityWeatherConditionServiceImpl.java
+++ airquality/src/main/java/finley/gmair/service/impl/CityWeatherConditionServiceImpl.java
@@ -30,94 +30,24 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-@PropertySource("classpath:moji.properties")
 public class CityWeatherConditionServiceImpl implements CityWeatherConditionService {
     private Logger logger = LoggerFactory.getLogger(CityWeatherConditionServiceImpl.class);
-
-    @Value("${base}")
-    private String base;
-
-    @Autowired
-    private LocationFeign locationFeign;
 
     @Autowired
     private MojiTokenService mojiTokenService;
 
-    private List<MojiRecord> list;
-
-    private Map<String, Province> provinces;
-
-    private Map<String, City> cities;
-
-    private Map<String, District> districts;
-
-    private MojiToken mojiToken;
+    @Autowired
+    public CityWeatherUtil cityWeatherUtil;
 
     @PostConstruct
     public void init() {
-        try {
-            File file = new File(base);
-            list = MojiRecordUtil.list(file);
-            logger.info("moji record: " + list.size());
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-        ResultData response = locationFeign.province();
-        if (response.getResponseCode() != ResponseCode.RESPONSE_OK) {
-            logger.error("获取省份信息失败");
-            return;
-        }
-        provinces = new HashMap<>();
-        cities = new HashMap<>();
-        districts = new HashMap<>();
-        JSONArray provinceArr = JSON.parseArray(JSON.toJSONString(response.getData()));
-        for (int i = 0; i < provinceArr.size(); i++) {
-            JSONObject item = provinceArr.getJSONObject(i);
-            String provinceId = item.getString("provinceId");
-            String provinceName = item.getString("provinceName");
-            String provincePinyin = item.getString("provincePinyin");
-            double longitude = item.getDouble("longitude");
-            double latitude = item.getDouble("latitude");
-            Province province = new Province(provinceId, provinceName, provincePinyin, longitude, latitude);
-            provinces.put(provinceId, province);
-            response = locationFeign.city(provinceId);
-            if (response.getResponseCode() != ResponseCode.RESPONSE_OK) {
-                logger.error("Fail to obtain city list under: " + provinceId);
-                continue;
-            }
-            JSONArray cityArr = JSON.parseArray(JSON.toJSONString(response.getData()));
-            for (int j = 0; j < cityArr.size(); j++) {
-                item = cityArr.getJSONObject(j);
-                String cityId = item.getString("cityId");
-                String cityName = item.getString("cityName");
-                String cityPinyin = item.getString("cityPinyin");
-                longitude = item.getDouble("longitude");
-                latitude = item.getDouble("latitude");
-                City city = new City(cityId, cityName, cityPinyin, longitude, latitude);
-                cities.put(cityId, city);
-                response = locationFeign.district(cityId);
-                if (response.getResponseCode() != ResponseCode.RESPONSE_OK) {
-                    logger.error("Fail to obtain district list under:  " + cityId);
-                    continue;
-                }
-                JSONArray districtArr = JSON.parseArray(JSON.toJSONString(response.getData()));
-                for (int k = 0; k < districtArr.size(); k++) {
-                    item = districtArr.getJSONObject(k);
-                    String districtId = item.getString("districtId");
-                    String districtName = item.getString("districtName");
-                    District district = new District(districtId, districtName);
-                    districts.put(districtId, district);
-                }
-            }
-        }
-        logger.info("province: " + provinces.size() + ", city: " + cities.size() + ", district: " + districts.size());
-        rank();
+        cityWeatherUtil.init();
     }
 
     private void rank() {
         new Thread(() -> {
             Map<String, CityWeatherCondition> map = new HashMap<>();
-            for (Map.Entry<String, Province> entry : provinces.entrySet()) {
+            for (Map.Entry<String, Province> entry : CityWeatherUtil.provinces.entrySet()) {
                 String provinceId = entry.getKey();
                 Province province = entry.getValue();
                 CityWeatherCondition weatherCondition = fetch(provinceId, province.getLongitude(), province.getLatitude());
@@ -128,7 +58,7 @@ public class CityWeatherConditionServiceImpl implements CityWeatherConditionServ
                 map.put(provinceId, weatherCondition);
             }
             logger.info("province weather condition complete");
-            for (Map.Entry<String, City> entry : cities.entrySet()) {
+            for (Map.Entry<String, City> entry : CityWeatherUtil.cities.entrySet()) {
                 String cityId = entry.getKey();
                 City city = entry.getValue();
                 CityWeatherCondition weatherCondition = fetch(cityId, city.getLongitude(), city.getLatitude());
@@ -140,7 +70,7 @@ public class CityWeatherConditionServiceImpl implements CityWeatherConditionServ
             }
             logger.info("city weather condition complete");
             try {
-                for (Map.Entry<String, District> entry : districts.entrySet()) {
+                for (Map.Entry<String, District> entry : CityWeatherUtil.districts.entrySet()) {
                     String districtId = entry.getKey();
                     District district = entry.getValue();
                     MojiRecord record = locate(district.getDistrictName());
@@ -169,48 +99,19 @@ public class CityWeatherConditionServiceImpl implements CityWeatherConditionServ
         ResultData result = new ResultData();
         MojiToken mojiToken = ((List<MojiToken>) selectToken().getData()).get(0);
         logger.info("Active moji token: " + JSON.toJSONString(mojiToken));
-        this.mojiToken = mojiToken;
+        CityWeatherUtil.mojiToken = mojiToken;
         rank();
         return result;
     }
 
     private CityWeatherCondition fetch(String lid, int cityId) {
-        StringBuffer stringBuffer = new StringBuffer(mojiToken.getUrl()).append("?cityId=").append(cityId);
-        String url = stringBuffer.toString();
-        String subUrl = url.substring("https://api.mojicb.com".length(), url.indexOf("?"));
-        logger.info(url);
-        Long timeValue = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
-        String timeStamp = timeValue.toString();
-        String nonce = UUID.randomUUID().toString();
-        String signature = Encryption.md5(mojiToken.getPassword() + "\n" + timeStamp + "\n" + nonce + "\n" + subUrl);
-        Map<String, String> map = new HashMap<>();
-        map.put("X-AC-Token", mojiToken.getToken());
-        map.put("X-Date", timeStamp);
-        map.put("X-AC-Nonce", nonce);
-        map.put("X-AC-Signature", signature);
-        String result = HttpDeal.getResponse(url, map);
-        logger.info("Request params: " + JSON.toJSONString(map));
+        String result = cityWeatherUtil.fetch(cityId);
         CityWeatherCondition weatherCondition = interpret(lid, result);
         return weatherCondition;
     }
 
     private CityWeatherCondition fetch(String lid, double longitude, double latitude) {
-//        MojiToken mojiToken = ((List<MojiToken>) selectToken().getData()).get(0);
-        StringBuffer stringBuffer = new StringBuffer(mojiToken.getUrl()).append("?lat=").append(latitude).append("&lon=").append(longitude);
-        String url = stringBuffer.toString();
-        String subUrl = url.substring("https://api.mojicb.com".length(), url.indexOf("?"));
-        logger.info(url);
-        Long timeValue = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
-        String timeStamp = timeValue.toString();
-        String nonce = UUID.randomUUID().toString();
-        String signature = Encryption.md5(mojiToken.getPassword() + "\n" + timeStamp + "\n" + nonce + "\n" + subUrl);
-        Map<String, String> map = new HashMap<>();
-        map.put("X-AC-Token", mojiToken.getToken());
-        map.put("X-Date", timeStamp);
-        map.put("X-AC-Nonce", nonce);
-        map.put("X-AC-Signature", signature);
-        String result = HttpDeal.getResponse(url, map);
-        logger.info("Request params: " + JSON.toJSONString(map));
+        String result = cityWeatherUtil.fetch(longitude, latitude);
         CityWeatherCondition weatherCondition = interpret(lid, result);
         return weatherCondition;
     }
@@ -283,14 +184,7 @@ public class CityWeatherConditionServiceImpl implements CityWeatherConditionServ
     }
 
     private MojiRecord locate(String district) {
-        logger.info("district: " + district + ", size: " + list.size());
-        for (int i = 0; i < list.size(); i++) {
-            MojiRecord record = list.get(i);
-            if (record.getName().contains(district)) {
-                return record;
-            }
-        }
-        return null;
+        return cityWeatherUtil.locate(district);
     }
 
 }
