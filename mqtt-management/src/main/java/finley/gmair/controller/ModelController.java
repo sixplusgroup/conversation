3e587c14023dfae76b16b6006827671aacec984d
lycheeshell <lycheeshell@163.com>
2020-12-12 16:37:28 +0800

    型号controller

--- mqtt-management/src/main/java/finley/gmair/controller/ModelController.java
+++ mqtt-management/src/main/java/finley/gmair/controller/ModelController.java
@@ -5,6 +5,7 @@ import finley.gmair.service.ModelService;
 import finley.gmair.util.ResultData;
 import finley.gmair.util.VerifyUtil;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class ModelController {
      * @return 数据库变化条数
      * @throws MqttBusinessException 异常
      */
-    @PostMapping(value = "/create")
+    @PostMapping(value = "/save")
     public ResultData saveModel(String name, String description) throws MqttBusinessException {
         VerifyUtil.verify(StringUtils.isNotEmpty(name), "型号标示名称为空");
         VerifyUtil.verify(StringUtils.isNotEmpty(description), "型号描述说明为空");
@@ -40,5 +41,48 @@ public class ModelController {
         return ResultData.ok(modelService.saveModel(name, description));
     }
 
+    /**
+     * 根据型号id查询不包含其行为的型号信息
+     *
+     * @param modelId 型号id
+     * @return 型号
+     * @throws MqttBusinessException 异常
+     */
+    @GetMapping(value = "/queryOne")
+    public ResultData queryOne(String modelId) throws MqttBusinessException {
+        VerifyUtil.verify(StringUtils.isNotEmpty(modelId), "型号id为空");
+
+        return ResultData.ok(modelService.queryOneWithoutAction(modelId));
+    }
+
+    /**
+     * 查询不包含其行为的型号信息
+     *
+     * @param name 型号标识名称的英文字符串
+     * @return 型号列表
+     * @throws MqttBusinessException 异常
+     */
+    @GetMapping(value = "queryModels")
+    public ResultData queryModels(String name) throws MqttBusinessException {
+        VerifyUtil.verify(StringUtils.isNotEmpty(name), "型号标示名称为空");
+
+        return ResultData.ok(modelService.queryModelsWithoutAction(name));
+    }
+
+    /**
+     * 新增型号与行为的对应关系
+     *
+     * @param modelId  型号id
+     * @param actionId 行为id
+     * @return 新增条数
+     * @throws MqttBusinessException 异常
+     */
+    @PostMapping(value = "/addModelAction")
+    public ResultData addModelActionRelation(String modelId, String actionId) throws MqttBusinessException {
+        VerifyUtil.verify(StringUtils.isNotEmpty(modelId), "型号id为空");
+        VerifyUtil.verify(StringUtils.isNotEmpty(actionId), "行为id为空");
+
+        return ResultData.ok(modelService.insertModelActionRelation(modelId, actionId));
+    }
 
 }
