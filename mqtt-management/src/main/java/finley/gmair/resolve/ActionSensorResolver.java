d138d9f791659b693f92444b484d31683f75059b
lycheeshell <lycheeshell@163.com>
2020-12-20 17:59:35 +0800

    sensor，bug待处理

---
+++ mqtt-management/src/main/java/finley/gmair/resolve/ActionSensorResolver.java
@@ -0,0 +1,69 @@
+package finley.gmair.resolve;
+
+import com.alibaba.fastjson.JSONObject;
+import finley.gmair.mqtt.MqttUtil;
+import finley.gmair.service.RedisService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * sensor行为的处理器
+ *
+ * @author lycheeshell
+ * @date 2020/12/19 12:36
+ */
+@Component
+public class ActionSensorResolver implements ActionResolver, InitializingBean {
+
+    @Resource
+    private RedisService redisService;
+
+    @Override
+    public void afterPropertiesSet() {
+        ActionResolverFactory.register("SENSOR", this);
+    }
+
+    /**
+     * 处理行为
+     *
+     * @param topic mqtt主题topic，topic的第一个"/"已经删除
+     * @param json  mqtt消息内容payload的json
+     */
+    @Override
+    public void resolve(String topic, JSONObject json) {
+        String[] topicArray = topic.split("/");
+        String machineId = topicArray[2];
+        String furtherAction;
+        if (topicArray.length == 6) { //7
+            //获取传感器数值类型（pm2.5, co2, temp, temp_out, humidity）其中之一
+            furtherAction = topicArray[6];
+            int value = (json.containsKey("value")) ? json.getIntValue("value") : Integer.MIN_VALUE;
+            dealSingleSensor(furtherAction, value);
+        } else if (topicArray.length == 7) { //6
+            MqttUtil.partial(redisService, machineId, json);
+        } else {
+
+        }
+    }
+
+    /**
+     * 处理单个传感器数据消息publish
+     */
+    private void dealSingleSensor(String detail_action, int value) {
+        if (detail_action.equals("pm2.5a")) {
+        }
+        if (detail_action.equals("pm2.5b")) {
+        }
+        if (detail_action.equals("co2")) {
+        }
+        if (detail_action.equals("temp")) {
+        }
+        if (detail_action.equals("temp_out")) {
+        }
+        if (detail_action.equals("humidity")) {
+        }
+    }
+
+}
