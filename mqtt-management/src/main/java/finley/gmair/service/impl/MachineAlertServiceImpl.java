cbfbb5d2bf2acf1b3e140a3498a3a436cfe0dd66
lycheeshell <lycheeshell@163.com>
2021-03-09 16:18:56 +0800

    取消事务

--- mqtt-management/src/main/java/finley/gmair/service/impl/MachineAlertServiceImpl.java
+++ mqtt-management/src/main/java/finley/gmair/service/impl/MachineAlertServiceImpl.java
@@ -6,7 +6,6 @@ import finley.gmair.service.MachineAlertService;
 import finley.gmair.util.IDGenerator;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
