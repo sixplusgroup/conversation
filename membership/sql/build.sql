5a85fa4aeb01cebe690aef83eb5bb6870ff2c635
xufan.zhang <sunshinezxf@hotmail.com>
2021-10-10 16:54:15 +0800

    refactor integral model

--- membership/sql/build.sql
+++ membership/sql/build.sql
@@ -7,39 +7,44 @@
 
 -- table: membership_consumer
 
-drop table if exists `membership_consumer`;
-create table `membership_consumer`(
-    `consumer_id` varchar(20) not null,
-    `first_integral` int not null default 0,
-    `second_integral` int not null default 0,
-    `create_time` datetime(0) NOT NULL,
-    primary key (`consumer_id`) using btree
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `membership_consumer`;
+CREATE TABLE `membership_consumer` (
+  `consumer_id`     VARCHAR(20) NOT NULL,
+  `first_integral`  INT         NOT NULL DEFAULT 0,
+  `second_integral` INT         NOT NULL DEFAULT 0,
+  `create_time`     DATETIME(0) NOT NULL,
+  PRIMARY KEY (`consumer_id`) USING BTREE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- table: integral_product
-drop table if exists `integral_product`;
-create table `integral_product`(
-    `product_id` varchar(20) not null,
-    `product_name` varchar(20) not null default '',
-    `create_time` datetime(0) NOT NULL,
-    primary key(`product_id`) using btree
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+DROP TABLE IF EXISTS `integral_product`;
+CREATE TABLE `integral_product` (
+  `product_id`   VARCHAR(20) NOT NULL,
+  `product_name` VARCHAR(20) NOT NULL DEFAULT '',
+  `create_time`  DATETIME(0) NOT NULL,
+  PRIMARY KEY (`product_id`) USING BTREE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- table: integral_record
-drop table if exists `integral_add`;
-create table `integral_add`(
-    `add_id` varchar(20) not null,
-    `consumer_id` varchar(20) not null,
-    `product_id` varchar(20) not null,
-    `integral_value` int not null default 0,
-    `is_confirmed` tinyint(1) not null default 0,
-    `confirmed_time` datetime(0),
-    `create_time` datetime(0) NOT NULL,
-    primary key (`add_id`) using btree,
-    foreign key (`consumer_id`) references `membership_consumer` (consumer_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    foreign key (`product_id`) references `integral_product` (product_id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `integral_add`;
+CREATE TABLE `integral_add` (
+  `add_id`         VARCHAR(20) NOT NULL,
+  `consumer_id`    VARCHAR(20) NOT NULL,
+  `integral_value` INT         NOT NULL DEFAULT 0,
+  `is_confirmed`   TINYINT(1)  NOT NULL DEFAULT 0,
+  `confirmed_time` DATETIME(0),
+  `create_time`    DATETIME(0) NOT NULL,
+  PRIMARY KEY (`add_id`) USING BTREE,
+  FOREIGN KEY (`consumer_id`) REFERENCES `membership_consumer` (consumer_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- event gmair_membership_integral_maintain
 -- show variables like '%event%'; 查看event_scheduler如果为OFF或0就表示关闭
@@ -47,8 +52,9 @@ create table `integral_add`(
 
 DROP EVENT IF EXISTS `gmair_membership_integral_maintain`;
 CREATE EVENT `gmair_membership_integral_maintain`
-    ON SCHEDULE
+  ON SCHEDULE
     EVERY '1' YEAR STARTS '2023-01-03 00:00:00'
-    ON COMPLETION PRESERVE
-    COMMENT 'integralmaintain'
-    DO update gmair_membership.membership_consumer set first_integral = second_integral, second_integral = 0;
+  ON COMPLETION PRESERVE
+  COMMENT 'integralmaintain'
+DO UPDATE gmair_membership.membership_consumer
+SET first_integral = second_integral, second_integral = 0;
