a2c076e2e167a7796eb228192dcef47e9dbc2dba
JobyTerry <1727177598@qq.com>
2021-10-22 22:54:33 +0800

    string to long

--- common/src/main/java/finley/gmair/param/membership/GiveIntegralParam.java
+++ common/src/main/java/finley/gmair/param/membership/GiveIntegralParam.java
@@ -1,5 +1,7 @@
 package finley.gmair.param.membership;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import finley.gmair.util.jsonSerialize.StringToLong;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -11,6 +13,7 @@ import javax.validation.constraints.Size;
 @Data
 public class GiveIntegralParam {
     @NotNull
+    @JsonDeserialize(using= StringToLong.class)
     private Long id;
     @NotNull
     private Integer integralValue;
