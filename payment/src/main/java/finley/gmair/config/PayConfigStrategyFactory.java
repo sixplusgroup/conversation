f80547291e4c5983946f6b582ea67b6aa5efccf9
JobyTerry <1727177598@qq.com>
2021-12-04 16:50:55 +0800

    DRIFTMP

--- payment/src/main/java/finley/gmair/config/PayConfigStrategyFactory.java
+++ payment/src/main/java/finley/gmair/config/PayConfigStrategyFactory.java
@@ -1,9 +1,6 @@
 package finley.gmair.config;
 
-import finley.gmair.bean.enums.PayClientType;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -22,18 +19,18 @@ public class PayConfigStrategyFactory {
     private Map<String,PayConfig> PAYCONFIG_STRATEGY_MAP = new HashMap<>();
     @PostConstruct
     private void initMap(){
-        PAYCONFIG_STRATEGY_MAP.put(PayConfigKey.OFFICIALACCOUNT,payConfigLoader.getOa());
+        PAYCONFIG_STRATEGY_MAP.put(PayConfigKey.DRIFTMP,payConfigLoader.getOa());
         PAYCONFIG_STRATEGY_MAP.put(PayConfigKey.SHOPMP,payConfigLoader.getMp());
     }
 
     public PayConfig getPayConfig(String payConfigKey){
         PayConfig payConfig = PAYCONFIG_STRATEGY_MAP.get(payConfigKey);
-        return payConfig == null ? PAYCONFIG_STRATEGY_MAP.get(PayConfigKey.OFFICIALACCOUNT) : payConfig;
+        return payConfig == null ? PAYCONFIG_STRATEGY_MAP.get(PayConfigKey.DRIFTMP) : payConfig;
         //默认是公众号配置, 防止发生空指针
     }
 
     private interface PayConfigKey{
-        String OFFICIALACCOUNT = "OFFICIALACCOUNT";
+        String DRIFTMP = "DRIFTMP";
         String SHOPMP = "SHOPMP";
     }
 }
