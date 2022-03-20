9e9e01c15bde1e03d6a560efcaf821de20dea96b
xidao <181250086@smail.nju.edu.cn>
2022-03-20 22:30:02 +0800

    some mapper.xml

--- knowledge-base/src/main/java/finley/gmair/dao/KnowledgeMapper.java
+++ knowledge-base/src/main/java/finley/gmair/dao/KnowledgeMapper.java
@@ -9,7 +9,18 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface KnowledgeMapper {
-    int insert(Knowledge knowledge);
+    void insert(Knowledge knowledge);
 
-    int delete(@Param("id") Integer id);
+    void delete(@Param("id") Integer id);
+
+    //可能考虑全部用modify?
+    void changeStatusTo2(@Param("id") Integer id);
+    void changeStatusTo1(@Param("id") Integer id);
+    void modify(Knowledge knowledge);
+
+    Knowledge getById(@Param("id") Integer id);
+
+
+
+
 }
