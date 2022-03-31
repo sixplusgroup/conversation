00a99852a8901fea8f93cb8d8e401b29e5454d4c
xidao <181250086@smail.nju.edu.cn>
2022-03-31 21:38:33 +0800

    fix bug

--- knowledge-base/src/main/java/finley/gmair/service/impl/CommentServiceImpl.java
+++ knowledge-base/src/main/java/finley/gmair/service/impl/CommentServiceImpl.java
@@ -10,10 +10,12 @@ import finley.gmair.model.knowledgebase.Comment;
 import finley.gmair.service.CommentService;
 import jdk.net.SocketFlow;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentMapper commentMapper;
