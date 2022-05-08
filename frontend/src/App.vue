3061c9f3fa55596ac0881fb0ae6789dc4ebbf44d
shen-shang66666 <1308469879@qq.com>
2022-05-08 21:02:27 +0800

    feat
    缓存

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,18 +15,18 @@ export default {
   components: {
     
   },
-  // created(){
-  //   //   在页面加载时读取sessionStorage里的状态信息
-  //   if ( sessionStorage.getItem('state') ) {
-  //     this.$store.replaceState( Object.assign( {}, this.$store.state,
-  //     JSON.parse(sessionStorage.getItem('state') ) ) )
-  //   }
+  created(){
+    //   在页面加载时读取sessionStorage里的状态信息
+    if ( sessionStorage.getItem('state') ) {
+      this.$store.replaceState( Object.assign( {}, this.$store.state,
+      JSON.parse(sessionStorage.getItem('state') ) ) )
+    }
 
-  //   //   页面刷新时将state数据存储到sessionStorage中
-  //   window.addEventListener('beforeunload',()=>{
-  //     sessionStorage.setItem('state',JSON.stringify(this.$store.state) )
-  //   })
-  // }
+    //   页面刷新时将state数据存储到sessionStorage中
+    window.addEventListener('beforeunload',()=>{
+      sessionStorage.setItem('state',JSON.stringify(this.$store.state) )
+    })
+  }
 }
 </script>
 
